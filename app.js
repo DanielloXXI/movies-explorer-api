55be70b29bd9585aa8951ff0ac71fb2783f2eec0
DanielloXXI <cybiran_ez@mail.ru>
2023-06-15 21:59:45 +0300

    Исправил имя БД в app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/', router);
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
-mongoose.connect('mongodb://127.0.0.1:27017/bitmoviesdb', {
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
 });
 
 app.listen(PORT, () => {
